1f8e6a17a021cb73a1a9147bf54095ba1a38d8d3
Tuan Anh <tp.anhh7@gmail.com>
2025-02-25 07:55:25 +0300

    Initial commit

---
+++ src/ts/domain/Movie.ts
@@ -0,0 +1,13 @@
+import Buyable from "./Buyable";
+
+export default class Movie implements Buyable {
+  constructor(
+    readonly id: number,
+    readonly name: string,
+    readonly price: number,
+    readonly country: string,
+    readonly slogan: string,
+    readonly genre: string,
+    readonly time: string
+  ) {}
+}
