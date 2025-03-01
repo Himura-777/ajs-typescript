ce37e3b43cc8f4397fdffec44cdb67e22052bd81
Tuan Anh <tp.anhh7@gmail.com>
2025-03-01 15:15:05 +0300

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,20 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: 'eslint:recommended',
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: "eslint:recommended",
   overrides: [
     {
-      files: ['**/*/*.test.ts'],
-      plugins: ['jest'],
-      extends: ['plugin:jest/recommended'],
-      rules: { 'jest/prefer-expect-assertions': 'off' }
-    }
+      files: ["**/*/*.test.ts"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended"],
+      rules: { "jest/prefer-expect-assertions": "off" },
+    },
   ],
-  rules: {}
-}
+  rules: {
+    "no-unused-vars": "off",
+  },
+};
